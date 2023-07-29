49fc0452a44ab310474f2d8d9718b05efac2bdea
Janet Blackquill <uhhadd@gmail.com>
2023-07-29 09:27:19 -0400

    minimal reproducible example!

--- src/main/scala/projekt/packagea/packagea.scala
+++ src/main/scala/projekt/packagea/packagea.scala
@@ -1,7 +1,8 @@
 package projekt.packagea
 
 import projekt.packageb.packageb
+import projekt.packageb.packageb_otherthing
 
-class packagea(using pb: packageb):
+class packagea(using pb: packageb, pbot: packageb_otherthing):
 	def hello(): Unit =
-		println(s"hello world from package a! ${pb}")
+		println(s"hello world from package a! ${pb} ${pbot}")
